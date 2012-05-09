36fdd168703b2f37d4559f8c08d8604ff47f8de4
Chris Conlon <chris@yassl.com>
2012-05-09 15:03:20 -0600

    fixed OS X build documentation

--- JavaBuild.sh
+++ JavaBuild.sh
@@ -9,15 +9,25 @@
 #        the setup of the development machine which this script is being run
 #        on.
 #
+#        If building on OS X:
+#
+#        1) The Java include directory is usually similar to:
+#           /System/Library/Frameworks/JavaVM.framework/Versions/A/Headers
+#        2) The shared library extension should be .dylib (libgsswrapper.dylib)
+#           instead of .so.
+#
 # Original source developed by yaSSL (http://www.yassl.com)
 # 
+
+# Create our package directory structure
+mkdir -p edu/mit/kerberos
 
 # Generate the SWIG GSS-API interface
 swig -java -package edu.mit.kerberos -outdir ./edu/mit/kerberos -o gsswrapper_wrap.c gsswrapper.i
 
 # Compile and link libgsswrapper.so
 gcc -c gsswrapper_wrap.c -I/usr/lib/jvm/java-6-openjdk/include -I/usr/local/include/gssapi
-gcc -shared gsswrapper_wrap.o -L/home/myuser/kerberos/src/lib -lgssapi_krb5 -lkrb5 -lk5crypto -lcom_err -lkrb5support -o libgsswrapper.so
+gcc -shared gsswrapper_wrap.o -L/usr/local/lib -lgssapi_krb5 -lkrb5 -lk5crypto -lcom_err -lkrb5support -o libgsswrapper.so
 
 # Build our example client and server
 javac Util.java client.java server.java
